f1d58b56f20b0fa2ea50d8afd8d695f65a049222
Anika Bandwar <bandwar.anika@gmail.com>
2023-07-17 14:34:38 -0400

    Initial Commit

---
+++ Harmonia/HarmoniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmoniaApp.swift
+//  Harmonia
+//
+//  Created by Anika on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HarmoniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
