13ac04901e7cd534f5d4f19675d0b322b25d8a88
Kayla <kan100307@gmail.com>
2023-07-20 11:15:27 -0400

    Completed log view without persisting data

--- Harmonia/LogView.swift
+++ Harmonia/LogView.swift
@@ -7,10 +7,72 @@
 
 import SwiftUI
 
+struct FeelingEntry: Identifiable {
+    let id = UUID()
+    var date: Date
+    var emoji: String
+}
+
 struct LogView: View {
+    @State private var date = Date()
+        @State private var emoji = ""
+        @State private var entries: [FeelingEntry] = []
     var body: some View {
-        Text("This is the log view!")
+        VStack {
+            Text("Daily Feelings Log")
+                .font(.title)
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.leading)
+                .padding()
+            Form {
+                Section(header: Text("Select Date")) {
+                    DatePicker(selection: $date, in: ...Date(), displayedComponents: .date) {
+                        Text("Date")
+                    }
+                }
+                
+                Section(header: Text("Enter Emoji")) {
+                    TextField("Emoji", text: $emoji)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+            }
+            Button(action: addEntry) {
+                Text("Add Entry")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+            List(entries) { entry in
+                            HStack {
+                                Text("\(entry.date, formatter: DateFormatter.date)")
+                                Spacer()
+                                Text(entry.emoji)
+                            }
+                        }
+
+            .padding()
+                
+            Spacer()
+            
+            
+        }
     }
+    private func addEntry() {
+        let entry = FeelingEntry(date: date, emoji: emoji)
+        entries.append(entry)
+        emoji = ""
+    }
+}
+
+extension DateFormatter {
+    static let date: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter
+    }()
 }
 
 struct LogView_Previews: PreviewProvider {
